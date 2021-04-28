66b179c033019634906585a8a257018d4c8c9608
unknown <edwardto@live.ca>
2021-04-27 18:42:39 -0700

    added some more Flask code

--- flaskapp/app.py
+++ flaskapp/app.py
@@ -1,13 +1,46 @@
+# Imported libraries
 from flask import Flask, render_template
+from flask import request
+import json
+import os
 
+# Flask app
 app = Flask(__name__)
+
+# Agnostic filepath for root directory
+site_root = os.path.realpath(os.path.dirname(__file__))
+json_url = os.path.join(site_root, 'subs.json')
 
 @app.route("/")
 def home():
     """
-    main page
+    Main route that displays homepage of web-app
     """
     return render_template('index.html')
+
+
+@app.route('/addsub', methods=['POST'])
+def addsub():
+    """
+    Writes subscription information to subs.json file
+    """
+    subdata = request.form['sub_data']
+    with open('subs.json') as json_file:
+        data = json.load(json_file)
+    with open('subs.json', mode='w') as f:
+        data.append(json.loads(subdata))
+        json.dump(data, f)
+    return f'', 200
+
+
+@app.route('/data', methods=['GET'])
+def data():
+    """
+    Hosts subscription data which can be retrieved in web-app
+    """
+    with open(json_url) as json_file:
+        data = json.load(json_file)
+    return f'{data}'
 
 if __name__ == "__main__":
     app.run()
---
+++ flaskapp/static/app.js
@@ -0,0 +1,4 @@
+// Method to send data from js to Flask (not sure if it works yet)
+// $.post( "/addsub", {
+//     sub_data: JSON.stringify(outputData)
+//   }
