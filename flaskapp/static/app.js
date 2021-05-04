a6fd47bdbd2aa5a589585c4f185ec12c18b0299a
Martin <martinlee5159@gmail.com>
2021-05-04 14:03:31 -0700

    add only unique name/cost subs

--- flaskapp/static/app.js
+++ flaskapp/static/app.js
@@ -25,15 +25,28 @@ submitBtn.addEventListener("click",newSubscription)
 // OUTPUT: none 
 function newSubscription(){
     let name = document.querySelector("#subscription-form").value
-    let cost = document.querySelector("#cost-form").value
+    let cost = parseFloat(document.querySelector("#cost-form").value)
     let period = getPeriod()
     let date = document.querySelector("#billedDate").value
-    listOfSubs.push(createSub(name,cost,period,date))
+
+    addToSubs(createSub(name,cost,period,date))
+}
+
+/* Given a subscription object, only push to listOfSubs global if it's unique */
+// INPUT: Subscription(object)
+// OUTPUT: none 
+function addToSubs(sub){
+    let exists = 0
+    for (e of listOfSubs){
+        if(e.name === sub.name && e.cost == sub.cost)
+        exists = 1
+    }
+    if (!exists) listOfSubs.push(sub)
 }
 
 /* Checks which radio button is selected and returns appropriate period as string */
 // INPUT: name(string) cost(float) period(string) date(Date)
-// OUTPUT: "yearly" or "monthly". "monthly" is default
+// OUTPUT: "yearly" "monthly"             "monthly" is default
 function getPeriod(){
     result = "monthly"
     let yearly = document.querySelector("#yearly").checked
@@ -41,12 +54,11 @@ function getPeriod(){
     if (yearly) result = "yearly"
     else if (monthly) result = "monthly"
     return result
-
 }
 
 /* Creates a Subscription Object and returns its reference. */
-// INPUT: name(string) cost(float) period(string) date(Date)
-// OUTPUT: Subscription (Object)
+// INPUT: name(string) cost(float) period(string) date(string)
+// OUTPUT: Subscription(object)
 function createSub(name, cost, period, date){
     let sub = {
         name,
@@ -58,9 +70,10 @@ function createSub(name, cost, period, date){
 }
 
 
-/* CGiven a subscription object, convert it to a JSON object and return */
+/* Given a Subscription object, convert it to a JSON object and return */
 // INPUT: Subscription (Object)
 // OUTPUT: JSON Object (string)
 function convertToJSON(subObject){
     return JSON.stringify(subObject)
 }
+
