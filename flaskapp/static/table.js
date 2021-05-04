ebaa950f9b496adeecdfaeaeb5582a42c0421b41
Martin <martinlee5159@gmail.com>
2021-05-04 16:31:35 -0700

    delete button now removes entry from table

--- flaskapp/static/table.js
+++ flaskapp/static/table.js
@@ -32,10 +32,37 @@ function insertEntry(sub){
 }
 
 /* Adds edit, notes, and delete button to table row  */
+// INPUT: <tr> (HTML)
+// OUTPUT: none 
 function addButtons(row){
     let cell = row.insertCell(4)
+    cell.appendChild(addDeleteButton())
     let deleteBtn = "<button id=delete-btn>delete</button>"
     let notesBtn = "<button onclick=\"window.location.href='note.html';\" id=notes-btn>notes</button>"
     let editBtn = "<button onclick=\"window.location.href='new.html';\" id=edit-btn>edit</button>"
-    cell.insertAdjacentHTML("afterbegin",deleteBtn + notesBtn + editBtn)
+    cell.insertAdjacentHTML("afterbegin",notesBtn + editBtn)
+}
+
+/* Creates a delete button and returns its reference */
+// INPUT: none
+// OUTPUT: <button> (HTML)
+function addDeleteButton(){
+    let button = document.createElement("button")
+    button.id = "delete-btn"
+    button.innerHTML = "delete"
+    button.addEventListener("click",removeRow)
+    return button
+}
+
+/* Deletes a subscription row according to click event */
+// INPUT: none
+// OUTPUT: none
+function removeRow(event){
+    let cell = event.target.parentElement
+    let row = cell.parentElement
+    table.deleteRow(row.rowIndex)
+}
+
+function removeSub(){
+    
 }
