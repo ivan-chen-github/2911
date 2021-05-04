902db18369fb6c6eaad345c6209194d409d48fe3
Martin <martinlee5159@gmail.com>
2021-05-04 15:56:48 -0700

    fixed jss & HTML for edit/add/notes buttons

--- flaskapp/static/table.js
+++ flaskapp/static/table.js
@@ -15,7 +15,7 @@ function updateTable(){
 
 }
 
-/* Inserts a row to the table for display */
+/* Inserts a row according to the given Subscription for the table */
 // INPUT: Subscription(object)
 // OUTPUT: none 
 function insertEntry(sub){
@@ -24,8 +24,18 @@ function insertEntry(sub){
     let nameCell = row.insertCell(1)
     let costCell = row.insertCell(2)
     let dateCell = row.insertCell(3)
-    statusCell.innerHTML = "Active" // Placeholder for now
+    statusCell.innerHTML = "Active" // Placeholder default for now
     nameCell.innerHTML = sub.name
     costCell.innerHTML = sub.cost.toFixed(2)
     dateCell.innerHTML = sub.date
+    addButtons(row)
+}
+
+/* Adds edit, notes, and delete button to table row  */
+function addButtons(row){
+    let cell = row.insertCell(4)
+    let deleteBtn = "<button id=delete-btn>delete</button>"
+    let notesBtn = "<button onclick=\"window.location.href='note.html';\" id=notes-btn>notes</button>"
+    let editBtn = "<button onclick=\"window.location.href='new.html';\" id=edit-btn>edit</button>"
+    cell.insertAdjacentHTML("afterbegin",deleteBtn + notesBtn + editBtn)
 }
