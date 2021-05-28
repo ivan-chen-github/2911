46be3e6f80d0dd5bfd1cd162987aed508ac4c0c7
unknown <edwardto@live.ca>
2021-05-27 23:55:27 -0700

    fixed /clear route unit test. all 15 tests passed

--- flaskapp/tests/test_route.py
+++ flaskapp/tests/test_route.py
@@ -123,11 +123,9 @@ def test_delsub_error(app, client):
     assert TypeError
 
 
-# def test_clear(app, client):
-#     """test if /clear successfully clears subs.json"""
-#     # test HTTP response, empty response
-#     res = client.post('/clear')
-#     # open subs.json and check that file has been reset to {"subs": []}
-#     with open(json_url, mode='r') as f:
-#         n = json.load(f)
-#         assert n == {"subs": []}
+def test_clear(app, client):
+    """test if /clear successfully deletes subs.json"""
+    # test HTTP response, empty response
+    res = client.delete('/clear')
+    # assert that subs.json doesn't exist
+    assert os.path.exists(json_url) == False
