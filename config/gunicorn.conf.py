321af9b716a10c3ec6a39eb498a3c0d38c4da6c1
Ivan <ivan15997533@gmail.com>
2021-11-27 17:54:46 -0800

    z

--- config/gunicorn.conf.py
+++ config/gunicorn.conf.py
@@ -2,4 +2,4 @@ def when_ready(server):
     open('/tmp/app-initialized', 'w').close()
 
 
-bind: '0.0.0.0:8080'
+bind: 'unix:///tmp/nginx.socket'
--- flaskapp/app.py
+++ flaskapp/app.py
@@ -217,4 +217,4 @@ def data():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=port)
